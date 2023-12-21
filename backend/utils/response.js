aa90f252f55fc6d99175ddc6d3719a43f75f0cbe
Dzulfikar Al Ghozali <dzulfikaralghozali9@gmail.com>
2023-12-21 15:40:02 +0700

    Boilerpath

--- backend/utils/response.js
+++
@@ -1,9 +0,0 @@
-const logger = require('./logger');
-
-exports.responseGenerator = (req, res, status, message, data) => {
-	logger.logResponse(req.id, res, status);
-	return res.status(status).json({
-		message,
-		data
-	});
-}
